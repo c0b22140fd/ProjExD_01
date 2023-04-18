33deb1e663cdda2751012a91fff0547d0a9892f3
setsuna <c0b22140fd@edu.teu.ac.jp>
2023-04-18 17:59:01 +0900

    演習課題

--- C0B22140.py
+++ C0B22140.py
@@ -6,10 +6,11 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_imgs = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
-    kk_imgc = pg.transform.rotozoom(kk_img, 10, 1.0) 
-    kk_imgs = [kk_img,kk_imgc]
+    kk_imgj = pg.transform.rotozoom(kk_img, 10, 1.0) 
+    kk_imgs = [kk_img,kk_imgj]
     tmr = 0
     x = 0
 
@@ -18,11 +19,19 @@ def main():
             if event.type == pg.QUIT: return
 
         tmr += 1
-        x = tmr
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [1600-x, 0])
-        screen.blit(kk_imgs[tmr%2], [300, 200])
+        x =tmr%3200
 
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_imgs, [1600-x, 0])
+        screen.blit(bg_img, [3200-x, 0])
+
+
+
+
+        if tmr%20 <=8:
+            screen.blit(kk_imgs[0], [300, 200])
+        elif tmr%20 >=8:
+            screen.blit(kk_imgs[1], [300, 200])
         pg.display.update()
         clock.tick(100)
 
